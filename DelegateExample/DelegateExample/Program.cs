fc44df25e64d81144ddf76c984af159e095771e3
Marek B <mareboro@o2.pl>
2023-09-03 22:40:13 +0100

    start of delegates

---
+++ DelegateExample/DelegateExample/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace DelegateExample;
+
+class Program
+{
+    delegate void LogDel(string text);
+
+    static void Main(string[] args)
+    {
+        LogDel logDel = new LogDel(LogTextToScreen);
+        logDel("some text");
+        Console.ReadKey();
+    }
+
+    static void LogTextToScreen(string text) {
+        Console.WriteLine($"{DateTime.Now}: {text}");
+    }
+
+}
